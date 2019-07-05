fc082936d6fb059b443832fabb9568d3afb11619
Timothy Myers <tim@ip-10-1-10-8.ec2.internal>
2019-07-05 16:33:30 -0600

    ADd tag

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -5,5 +5,15 @@ yarn build
 
 echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' >> .npmrc
 echo 'registry=http://registry.npmjs.org' >> .npmrc
-pwd
-VERSION=`./scripts/get-version` && yarn publish --access public
+
+VERSION=`./scripts/get-version` && sed -i.bak "s/\$${VERSION}/$(VERSION)/g" ./package.json
+NPM_TAG="dev"
+
+# If the package doesn't have a pre-release tag, use the tag of latest instead of
+# dev. NPM uses this tag as the default version to add, so we want it to mean
+# the newest released version.
+if [[ $(jq -r .version < package.json) != *-* ]]; then
+    NPM_TAG="latest"
+fi
+
+yarn publish --tag "${NPM_TAG}"
