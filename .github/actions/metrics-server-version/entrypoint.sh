e916655d7c8e88094573ce69e2aab05dc1b5439c
Timothy Myers <tim@Timothys-MacBook-Pro.local>
2019-10-17 08:31:46 -0600

    Try creating PR

--- .github/actions/metrics-server-version/entrypoint.sh
+++ .github/actions/metrics-server-version/entrypoint.sh
@@ -2,5 +2,5 @@
 
 export NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq '.[0].tag_name'`
 cat src/index.ts
-sed -ie 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '"$NEW_VERSION"';/' src/index.ts
+sed -ie 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '\'$NEW_VERSION\'';/' src/index.ts
 cat src/index.ts
