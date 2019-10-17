497bee16b9bf0f416eb0141ac12d31fa41cdc14a
Timothy Myers <tim@Timothys-MacBook-Pro.local>
2019-10-17 08:42:18 -0600

    Remove file

--- .github/actions/metrics-server-version/entrypoint.sh
+++ .github/actions/metrics-server-version/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-export NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq '.[0].tag_name'`
-cat src/index.ts
+export NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq -r '.[0].tag_name'`
 sed -ie 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '\'$NEW_VERSION\'';/' src/index.ts
-cat src/index.ts
+rm src/index.tse || true
