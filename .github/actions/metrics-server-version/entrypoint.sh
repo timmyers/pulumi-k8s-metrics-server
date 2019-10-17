95219fa77da829761e3fe52c6dbfcc5d926ae625
Timothy Myers <tim@Timothys-MacBook-Pro.local>
2019-10-17 09:30:16 -0600

    pass data

--- .github/actions/metrics-server-version/entrypoint.sh
+++ .github/actions/metrics-server-version/entrypoint.sh
@@ -2,4 +2,4 @@
 
 NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq -r '.[0].tag_name'`
 sed -i -e 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '\'$NEW_VERSION\'';/' src/index.ts
-echo ::set-env name=METRICS_SERVER_NEW_VERSION::$NEW_VERSION
+echo ::set-output name=new_version::$NEW_VERSION
