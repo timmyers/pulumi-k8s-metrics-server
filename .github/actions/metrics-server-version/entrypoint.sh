a8119734c3a3bda1f9504ed84c03eaae7ee55215
Timothy Myers <tim@Timothys-MacBook-Pro.local>
2019-10-17 09:09:31 -0600

    ci: try getting latest metrics-server release

---
+++ .github/actions/metrics-server-version/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -l
+
+NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq -r '.[0].tag_name'`
+sed -i -e 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '\'$NEW_VERSION\'';/' src/index.ts
+echo ::set-output name=new_version::$NEW_VERSION
--- src/index.ts
+++ src/index.ts
@@ -60,17 +60,17 @@ const defaults = (args: MetricsServerArgs): MetricsServerArgs => {
     args.podDisruptionBudget = { enabled: false };
   }
 
-  const imageTag = 'v0.3.6';
+  const metricsServerImageTag = 'v0.3.5';
 
   if (args.image === undefined) {
     args.image = { 
       repository: 'gcr.io/google_containers/metrics-server-amd64',
-      tag: imageTag,
+      tag: metricsServerImageTag,
       pullPolicy: 'IfNotPresent',
     }
   } else {
     if (args.image.repository === undefined) args.image.repository = 'gcr.io/google_containers/metrics-server-amd64';
-    if (args.image.tag === undefined) args.image.tag = imageTag;
+    if (args.image.tag === undefined) args.image.tag = metricsServerImageTag;
     if (args.image.pullPolicy === undefined) args.image.pullPolicy = 'IfNotPresent';
   }
 
