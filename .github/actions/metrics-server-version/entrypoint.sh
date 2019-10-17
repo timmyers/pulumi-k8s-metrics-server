9f853416680d4834babeb633254c094cae68cda1
Timothy Myers <tim@Timothys-MacBook-Pro.local>
2019-10-17 09:09:31 -0600

    ci: try getting latest metrics-server release

---
+++ .github/actions/metrics-server-version/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -l
+
+NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq -r '.[0].tag_name'`
+sed -i -e 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '\'$NEW_VERSION\'';/' src/index.ts
