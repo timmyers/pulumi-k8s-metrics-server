9356460800635122ea3ba2fa61f861c80d0befcd
Timothy Myers <tim@Timothys-MacBook-Pro.local>
2019-10-17 08:46:36 -0600

    fixes

--- .github/actions/metrics-server-version/entrypoint.sh
+++ .github/actions/metrics-server-version/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -l
 
-export NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq -r '.[0].tag_name'`
-sed -ie 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '\'$NEW_VERSION\'';/' src/index.ts
-rm src/index.tse || true
+NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq -r '.[0].tag_name'`
+sed -i -e 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '\'$NEW_VERSION\'';/' src/index.ts
