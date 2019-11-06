d0c2bc6961ffa685e04d4fc72fab44ff8d6336eb
Timothy Myers <tim@ip-10-1-10-28.ec2.internal>
2019-11-06 16:37:00 -0700

    ci: fix releases curl cmd

--- .github/actions/metrics-server-version/entrypoint.sh
+++ .github/actions/metrics-server-version/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-NEW_VERSION=`curl https://api.github.com/repos/kubernetes-incubator/metrics-server/releases | jq -r '.[0].tag_name'`
+NEW_VERSION=`curl https://api.github.com/repositories/92132038/releases | jq -r '.[0].tag_name'`
 sed -i -e 's/const metricsServerImageTag = '\'.\*\'';/const metricsServerImageTag = '\'$NEW_VERSION\'';/' src/index.ts
 echo ::set-output name=new_version::$NEW_VERSION
